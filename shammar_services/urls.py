a10945c6a4f3c87ddd5a3748d2716e345106a7bc
chadrackmbupiokajess <chadrackmbupiokajess@gmail.com>
2026-03-28 08:43:44 +0100

    ajout de PWA

--- shammar_services/urls.py
+++ shammar_services/urls.py
@@ -5,10 +5,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mabipint.urls')),
+    # PWA: Servir le service worker à la racine
+    path('service-worker.js', TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript'), name='service-worker'),
 ]
 
 if settings.DEBUG:
---
+++ static/service-worker.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'shammar-services-cache-v1';
+const urlsToCache = [
+    '/',
+    '/static/manifest.json',
+    '/static/css/main.css', /* Si vous avez un fichier CSS principal */
+    '/static/img/icon-192x192.png',
+    '/static/img/icon-512x512.png',
+    /* Ajoutez ici toutes les URLs de vos assets statiques importants */
+    /* Par exemple:
+    '/static/js/main.js',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    */
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
